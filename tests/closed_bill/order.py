3f65d3d3d453aadbd9a526e1287e3904d683277c
Bagusosh <bagusosh@gmail.com>
2022-01-03 14:57:28 +0700

    Closed Bill : Order Test Module

--- tests/closed_bill/order.py
+++ tests/closed_bill/order.py
@@ -63,6 +63,7 @@ class EmployeeJobPositionTests(unittest.TestCase):
         self.payment_method_row_xpath = '//*[@id="root"]/div/div/div/div[4]/div'
         self.order_success_page_xpath = '//*[@id="root"]/div/div[1]/div'
         self.payment_method_list_xpath = '//*[@id="root"]/div/div/div/div[4]'
+        self.note_field_xpath = '/html/body/div[2]/div[3]/div/form/div[2]/div/textarea'
 
         self.button_add_menu_shopping_cart_xpath = '/html/body/div[2]/div[3]/div/div/button'
         self.button_cart_order_xpath = '//*[@id="root"]/div/div/div[2]/div[2]/div[3]/div/span/div'
@@ -71,6 +72,7 @@ class EmployeeJobPositionTests(unittest.TestCase):
         self.button_login_account_xpath = '//*[@id="root"]/div/div/div/div[1]/form/div[4]/button'
         self.button_continue_register_xpath = '//*[@id="root"]/div/div/div/div[1]/form/div[7]/button'
         self.button_order_and_pay_xpath = '//*[@id="root"]/div/div/div/button/span'
+        self.button_save_note_xpath = '/html/body/div[2]/div[3]/div/form/div[3]/button'
 
         # etc
         self.context = {}
@@ -600,6 +602,134 @@ class EmployeeJobPositionTests(unittest.TestCase):
 
             assert page_exist is True
             logger.success("Order Menu Register First Test Case has been Tested")
+
+    def test_order_menu_add_notes(self):
+        with self.driver as driver:
+            # filter category
+            category_row_elements = driver.find_elements(By.XPATH, self.category_row_xpath)
+            for row_element in category_row_elements:
+                if row_element.find_element(By.TAG_NAME, 'span').text == self.first_category_name:
+                    row_element.click()
+
+            # add menu
+            menu_row_elements = driver.find_elements(By.XPATH, self.first_menu_row_xpath)
+            for row_element in menu_row_elements:
+                if row_element.find_element(By.TAG_NAME, 'h5').text == self.valid_first_menu_name:
+                    row_element.find_element(By.TAG_NAME, 'span').click()
+                    break
+
+            for row_element in menu_row_elements:
+                if row_element.find_element(By.TAG_NAME, 'span').text == 'Catatan':
+                    row_element.find_element(By.TAG_NAME, 'button').click()
+
+            driver.find_element(By.XPATH, self.note_field_xpath).send_keys('Pakai cabai 10 biji')
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, self.button_save_note_xpath))
+                ).click()
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, self.button_cart_order_xpath))
+                ).click()
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.text_to_be_present_in_element((By.TAG_NAME, 'h3'), 'Konfirmasi Order')
+                )
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            # if we not use time.sleep, element for button login cant be clicked, because the element has animation
+            time.sleep(2)
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, self.button_login_xpath)),
+                    EC.element_to_be_clickable((By.XPATH, self.button_login_xpath))
+                ).click()
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            driver.find_element(By.ID, 'hp').send_keys('087741331517')
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, self.button_login_account_xpath))
+                ).click()
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            data = requests.post('https://staging.flick.id/v1/users/auth/verifikasi-token/login?hp=087741331517&tipeUser=user').json()
+            token = data['data']['token']
+
+            driver.find_element(By.ID, 'otp').send_keys(token)
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, self.button_login_account_xpath))
+                ).click()
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.ID, 'password'))
+                ).send_keys(150600)
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, self.button_login_account_xpath))
+                ).click()
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
+            # if we use webdriverwait, cant click button payment method
+            time.sleep(2)
+
+            driver.find_element(By.XPATH, '//*[@id="Flick Pay"]').click()
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, self.button_order_and_pay_xpath))
+                ).click()
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                return
+
+            # for handle alert prompts
+            alert = Alert(driver)
+            alert.accept()
+
+            try:
+                _ = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, self.order_success_page_xpath))
+                )
+                page_exist = True
+            except TimeoutException:
+                logger.error("Order Menu Add Notes Test Case Resulted Error")
+                page_exist = False
+                return
+
+            assert page_exist is True
+            logger.success("Order Menu Add Notes Test Case has been Tested")
 
     def tearDown(self) -> None:
         pass
